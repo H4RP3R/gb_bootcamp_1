caf9980304ad5a2a4c9ab4b64297ce9bc9e00213
zombiehunter <viktormallory25@gmail.com>
2023-10-13 22:24:50 +0300

    mind fucked with lesson 4

---
+++ Lesson4/ex1/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+for (int n = 0; n < 90; n++)
+{
+    int count = 0;
+    int i = n;
+    while (i > 0)
+    {
+        count++;
+        s += i;
+        i /= 2; // log(n)+1
+    }
+    Console.WriteLine($"n:{n} count:{count}");
+}
+*/
+
+int n = Random.Shared.Next(10000000);
+double s = 0;
+int i = n;
+
+for (int j = 2; j < n / 2; j++)
+{
+    while (i > 0)
+    {
+        s += i;
+        i /= 2;
+    }
+}
+
+// (n/2-2) * (log(n)+1) => O(n log(n))
---
+++ Lesson4/ex2/Program.cs
@@ -0,0 +1,58 @@
+﻿// Сортировка выбором
+// На каждом i-ом шаге алгоритма находим минимальный элемент и меняем его местами с i-ым элементом в массиве.
+
+// a1 a2 a3 a4 ... an   n-1 действия
+//    a2 a3 a4 ... an   n-2
+//       a3 a4 ... an   n-3
+//            .....     n-4
+//                      n-5
+//                      ...
+//                       3
+//                       2 действия
+//                       1 действие
+//      
+// 1 + 2 + 3 + ... + (n-3) + (n-2) + (n-1) => n * n/2 => n^2/2 => O(n^2)
+//
+// 1 + n - 2 -> n
+// 2 + n - 2 -> n
+// ...
+// 
+// 1    9   n/2
+// 2    8
+// 3    7
+// 4    6
+// 5    5 
+
+// Сложность O(n^2)      
+
+// 7 6 3 4 5 1 2 3 <| 0
+// 1 6 3 4 5 7 2 3 <| 1
+// 1 2 3 4 5 7 6 3 <| 2
+// 1 2 3 4 5 7 6 3 <| 3
+// 1 2 3 3 5 7 6 4 <| 4
+// 1 2 3 3 4 7 6 5 <| 5
+// 1 2 3 3 4 5 6 7 <| 6
+// 1 2 3 3 4 5 6 7 <| 7
+// 1 2 3 3 4 5 6 7 <| 8
+
+int[] SortSelection(int[] collection)
+{
+    int size = collection.Length;
+    for (int i = 0; i < size; i++)
+    {
+        int pos = i;
+        for (int j = i + 1; j < size; j++)
+        {
+            if (collection[j] < collection[pos]) pos = j;
+        }
+        int temp = collection[i];
+        collection[i] = collection[pos];
+        collection[pos] = temp;
+    }
+    return collection;
+}
+
+
+int[] array = new int[] { 7, 6, 3, 4, 5, 1, 2, 3 };
+Console.WriteLine(string.Join(", ", array));
+Console.WriteLine(string.Join(", ", SortSelection(array)));
