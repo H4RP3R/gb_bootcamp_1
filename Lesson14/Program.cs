8ba59e37033cbdfe72a6639a8fefaa19265d515d
zombiehunter <viktormallory25@gmail.com>
2023-10-23 16:55:06 +0300

    Lesson14 added

---
+++ Lesson14/Program.cs
@@ -0,0 +1,95 @@
+﻿const int THREADS_NUMBER = 8; //число потоков
+const int N = 100_000; //размер массива
+object locker = new();
+
+void PrepareParallelCountingSort(int[] inputArray)
+{
+    int max = inputArray.Max();
+    int min = inputArray.Min();
+
+    int offset = -min;
+    int[] counters = new int[max + offset + 1];
+
+    int eachThreadCalc = N / THREADS_NUMBER; // кол-во вычислений на каждом потоке
+    var threadsParall = new List<Thread>();
+
+    for (int i = 0; i < THREADS_NUMBER; i++)
+    {
+        int startPos = i * eachThreadCalc;
+        int endPos = (i + 1) * eachThreadCalc;
+        if (i == THREADS_NUMBER - 1) endPos = N;
+        threadsParall.Add(new Thread(() => CountingSortParallel(inputArray, counters, offset, startPos, endPos)));
+        threadsParall[i].Start();
+    }
+
+    foreach (var thread in threadsParall)
+    {
+        thread.Join();
+    }
+
+    int index = 0;
+    for (int i = 0; i < counters.Length; i++)
+    {
+        for (int j = 0; j < counters[i]; j++)
+        {
+            inputArray[index] = i - offset;
+            index++;
+        }
+    }
+}
+
+void CountingSortParallel(int[] inputArray, int[] counters, int offset, int startPos, int endPos)
+{
+    for (int i = startPos; i < endPos; i++)
+    {
+        lock (locker) // Защита от возможной записи одной ячейки памяти с нескольких потоков
+        {
+            counters[inputArray[i] + offset]++;
+        }
+    }
+}
+
+void CountingSortExtended(int[] inputArray)
+{
+    int max = inputArray.Max();
+    int min = inputArray.Min();
+
+    int offset = -min;
+    int[] counters = new int[max + offset + 1];
+
+
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        counters[inputArray[i] + offset]++;
+    }
+
+    int index = 0;
+    for (int i = 0; i < counters.Length; i++)
+    {
+        for (int j = 0; j < counters[i]; j++)
+        {
+            inputArray[index] = i - offset;
+            index++;
+        }
+    }
+}
+
+bool EqualityMatrix(int[] fMatrix, int[] sMatrix)
+{
+    bool res = true;
+
+    for (int i = 0; i < N; i++)
+    {
+        res = res && (fMatrix[i] == sMatrix[i]);
+    }
+    return res;
+}
+
+Random rand = new();
+int[] resSerial = new int[N].Select(r => rand.Next(0, 5)).ToArray();
+int[] resParallel = new int[N];
+Array.Copy(resSerial, resParallel, N);
+
+CountingSortExtended(resSerial);
+PrepareParallelCountingSort(resParallel);
+Console.WriteLine($"Arrays are equal: {EqualityMatrix(resSerial, resParallel)}");
