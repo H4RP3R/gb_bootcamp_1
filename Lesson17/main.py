bfdeb4edf6e5456e1af3e5292b5675ab13f7d0e5
zombiehunter <viktormallory25@gmail.com>
2023-10-28 18:11:05 +0300

    Lesson17 added

---
+++ Lesson17/main.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+app.static_folder = 'static'
+
+
+@app.route('/', methods=['get'])
+def main():
+    return render_template('index.html', title='Main Page')
+
+
+@app.route('/info', methods=['get'])
+def info():
+    students = ['Ivan', 'Lisa', 'Mark', 'Tom', 'Ann', 'Donna', 'Mary']
+    return render_template('info.html', title='Info', names=students)
+
+
+@app.route('/condt/<x>/<y>')
+def summ(x, y):
+    return render_template('condition.html', title='Compare Numbers', x=int(x), y=int(y))
+
+
+if __name__ == '__main__':
+    app.run()
